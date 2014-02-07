db272856f38f33069e226b7abc4e57f0a915a2f5
Leo Gutierrez <photog_friend@yahoo.com>
2014-02-07 07:21:27 -0800

    Assignment 5 HW prob 1
    Convert Mar Sort program to use dynamic array and pointer notation only

---
+++ MarkSort_Converted/main.cpp
@@ -0,0 +1,113 @@
+/*
+* File: main.cpp
+* Author: Leo Gutierrez
+* Created on February 3, 2014, 8:13 AM
+* 
+* Re write using pointer notation
+*/
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+//void filAray(int *, int);
+int *filAray(int);
+void prntAry(int *,int,int);
+void swap(int &,int &);
+void swap(int *,int,int);
+void minPos(int *,int,int);
+void mrkSort(int *,int);
+//void xcopy(const int *,int *,int);
+int *xcopy(const int *,int);
+
+//Executions Begin Here!
+int main() {
+    //Declare variables and initialize the
+    const int SIZE=200;
+    int *array;
+    int *brray;
+    
+    //See random number generator
+    srand(static_cast<unsigned int>(time(0)));
+    
+    //Fill the arrays
+    array = filAray(SIZE);
+    brray = xcopy(array, SIZE);
+    
+    //Print the array
+    prntAry(array,SIZE,10);
+    
+    //Test out the min pos routine
+    mrkSort(brray,SIZE);
+    
+    //Print the array
+    prntAry(brray,SIZE,10);
+    
+    //Clear array
+    delete [] array;
+    delete [] brray;
+    
+    //Exit Stage Right!!!
+    return 0;
+}
+
+//void xcopy(const int *a,int *b,int n){
+int *xcopy(const int *a, int n){
+    int *brray = new int[n];
+    for(int i = 0; i < n; i++){
+        *(brray+i) = *(a+i);
+    }
+    
+    return brray;
+}
+
+void mrkSort(int *a,int n){
+    for(int i = 0; i < n - 1; i++){
+        minPos(a, n, i);
+    }
+}
+
+void minPos(int *a, int n, int pos){
+    for(int i = pos + 1; i < n; i++){
+        if(a[pos] > *(a+i))
+            swap(a[pos], *(a+i));
+        //if(a[pos]>a[i])swap(a,pos,i);
+    }
+}
+
+void swap(int &a,int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap(int *a,int i,int j){
+    int temp=*(a+1);
+    *(a+1)=*(a+j);
+    *(a+j)=temp;
+}
+
+void prntAry(int *a,int n,int perLine){
+    cout << endl;
+    for(int i = 0; i < n; i++){
+        cout << *(a+i) << " ";
+        if(i % 10 == (perLine - 1))
+            cout << endl;
+    }
+    cout<<endl;
+}
+
+//2 Digit random numbers
+int *filAray(int n){
+    int *array = new int[n];
+    for(int i = 0; i < n; i++){
+        *(array+i) = rand() % 90 + 10;
+    }
+    
+    return array;
+}
