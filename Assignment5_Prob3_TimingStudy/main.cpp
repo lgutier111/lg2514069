f8bb4dfb3fd612676a2b7328a858aa4603a19df3
Leo Gutierrez <photog_friend@yahoo.com>
2014-02-07 20:48:16 -0800

    Assignment 5 HW Prob 3
    Timing Study program with spreadsheet

---
+++ Assignment5_Prob3_TimingStudy/main.cpp
@@ -0,0 +1,152 @@
+/*
+* File: main.cpp
+* Author: Leo Gutierrez
+* Created on February 3, 2014, 8:13 AM
+* 
+* Change this code to use a bubble sort and a selection sort and plot
+*   how long the sort takes in comparison to other sorts.
+* 
+*/
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+void filAray(int [],int);
+void prntAry(const int [],int,int);
+void swap(int &,int &);
+void swap(int [],int,int);
+void minPos(int [],int,int);
+void mrkSort(int [],int);
+void bubSort(int [],int);
+void selSort(int [],int);
+void xcopy(const int [],int [],int);
+
+//Executions Begin Here!
+int main() {
+    //Open a file to record the data
+    ofstream output;
+    output.open("TimeStudy.dat");
+    
+    //Declare variables and initialize the
+    //random number generator
+    const int SIZE=150000;
+    int array[SIZE],brray[SIZE];
+    srand(static_cast<unsigned int>(time(0)));
+    
+    //Fill the arrays
+    filAray(array,SIZE);
+    
+    //Print the array
+    //prntAry(array,SIZE,10);
+    //Test out the min pos routine
+    for(int size=10000;size<SIZE;size+=10000){
+        xcopy(array,brray,size);
+        int strTime=time(0);
+//      mrkSort(brray,size);
+//      bubSort(brray,size);
+        selSort(brray,size);        
+        int endTime=time(0);
+        cout<<"Total Time Taken = "<<endTime-strTime
+                <<" secs for array size = "<<size<<endl;
+        output<<size<<" "<<endTime-strTime<<endl;
+    }
+    
+    //Print the array
+    //prntAry(brray,SIZE,10);
+    //Exit Stage Right!!!
+    output.close();
+    return 0;
+}
+
+void xcopy(const int a[],int b[],int n){
+    for(int i=0;i<n;i++){
+        b[i]=a[i];
+    }
+}
+
+void mrkSort(int a[],int n){
+    for(int i=0;i<n-1;i++){
+        minPos(a,n,i);
+    }
+}
+
+void bubSort(int a[], int n){
+    bool swap;
+    int temp;
+    
+    do{
+        swap = false;
+        for (int count = 0; count < (n - 1); count++)
+        {
+            if (a[count] > a[count + 1])
+            {
+                temp = a[count];
+                a[count] = a[count + 1];
+                a[count + 1] = temp;
+                swap = true;
+            }
+        }
+    }while (swap);
+}
+
+void selSort (int a[], int n){
+    int startScan, minIndex, minValue;
+    
+    for (startScan = 0; startScan < (n - 1); startScan++)
+    {
+        minIndex = startScan;
+        minValue = a[startScan];
+        for (int index = startScan + 1; index < n; index++)
+        {
+            if (a[index] < minValue)
+            {
+                minValue = a[index];
+                minIndex = index;
+            }
+        }
+        a[minIndex] = a[startScan];
+        a[startScan] = minValue;
+    }
+}
+
+void minPos(int a[],int n,int pos){
+    for(int i=pos+1;i<n;i++){
+        if(a[pos]>a[i])swap(a[pos],a[i]);
+        //if(a[pos]>a[i])swap(a,pos,i);
+    }
+}
+
+void swap(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+
+void swap(int a[],int i,int j){
+    int temp=a[i];
+    a[i]=a[j];
+    a[j]=temp;
+}
+
+void prntAry(const int a[],int n,int perLine){
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+        if(i%10==(perLine-1))cout<<endl;
+    }
+    cout<<endl;
+}
+
+//2 Digit random numbers
+void filAray(int a[],int n){
+    for(int i=0;i<n;i++){
+        a[i]=rand()%90+10;
+    }
+}
---
+++ Assignment5_Prob3_TimingStudy/nbproject/Package-Release.bash
@@ -0,0 +1,76 @@
+#!/bin/bash -x
+
+#
+# Generated - do not edit!
+#
+
+# Macros
+TOP=`pwd`
+CND_PLATFORM=Cygwin_4.x-Windows
+CND_CONF=Release
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+CND_DLIB_EXT=dll
+NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
+TMPDIRNAME=tmp-packaging
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/assignment5_prob3_timingstudy
+OUTPUT_BASENAME=assignment5_prob3_timingstudy
+PACKAGE_TOP_DIR=assignment5prob3timingstudy/
+
+# Functions
+function checkReturnCode
+{
+    rc=$?
+    if [ $rc != 0 ]
+    then
+        exit $rc
+    fi
+}
+function makeDirectory
+# $1 directory path
+# $2 permission (optional)
+{
+    mkdir -p "$1"
+    checkReturnCode
+    if [ "$2" != "" ]
+    then
+      chmod $2 "$1"
+      checkReturnCode
+    fi
+}
+function copyFileToTmpDir
+# $1 from-file path
+# $2 to-file path
+# $3 permission
+{
+    cp "$1" "$2"
+    checkReturnCode
+    if [ "$3" != "" ]
+    then
+        chmod $3 "$2"
+        checkReturnCode
+    fi
+}
+
+# Setup
+cd "${TOP}"
+mkdir -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package
+rm -rf ${NBTMPDIR}
+mkdir -p ${NBTMPDIR}
+
+# Copy files and create directories and links
+cd "${TOP}"
+makeDirectory "${NBTMPDIR}/assignment5prob3timingstudy/bin"
+copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
+
+
+# Generate tar file
+cd "${TOP}"
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/assignment5prob3timingstudy.tar
+cd ${NBTMPDIR}
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/assignment5prob3timingstudy.tar *
+checkReturnCode
+
+# Cleanup
+cd "${TOP}"
+rm -rf ${NBTMPDIR}
